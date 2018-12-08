3c6c56a8ad2fadd41bfd5ebfb137d1f21550a166
Rafael S. L <rafael.souza.lana@gmail.com>
2018-12-08 17:49:58 -0200

    Ajuste nos exemplos

--- src/public/exemplos/ex1.js
+++ src/public/exemplos/ex1.js
@@ -16,12 +16,16 @@ export const restricoes = [
   '24x1 + 30x2 <= 360',
 ]
 
-export const cabecalhoTopo = ['x1', 'x2', 'f1', 'f2', 'f3', 'b'];
+export const cabecalhoTopo = ['x1', 'x2', 'f1', 'f2', 'b'];
 export const variaveis = cabecalhoTopo.filter(item => item.includes('x'))
-export const cabecalhoEsquerda = ['f1', 'f2', 'f3', 'z'];
+export const cabecalhoEsquerda = ['f1', 'f2', 'z'];
 export const simplex = [
-  [1, 0, 1, 0, 0, 4],
-  [0, 2, 0, 1, 0, 12],
-  [2, 3, 0, 0, 1, 21],
-  [-3, -5, 0, 0, 0, 0]
+  [40, 25, 1, 0, 400],
+  [24, 30, 0, 1, 360],
+  [-520, -450, 0, 0, 0]
 ]
+
+// Solução:
+// x1 = 5
+// x2 = 8
+// z = 6200
--- src/public/exemplos/ex2.js
+++ src/public/exemplos/ex2.js
@@ -7,11 +7,18 @@ export const restricoes = [
   //x1 >= 0 e x2 >= 0
 ]
 
-export const cabecalhoTopo = ['x1', 'x2', 'f1', 'f2', 'b'];
+export const cabecalhoTopo = ['x1', 'x2', 'f1', 'f2', 'f3', 'b'];
 export const variaveis = cabecalhoTopo.filter(item => item.includes('x'))
-export const cabecalhoEsquerda = ['f1', 'f2', 'z'];
+export const cabecalhoEsquerda = ['f1', 'f2', 'f3', 'z'];
 export const simplex = [
-  [40, 25, 1, 0, 400],
-  [24, 30, 0, 1, 360],
-  [-520, -450, 0, 0, 0]
+  [1, 0, 1, 0, 0, 4],
+  [0, 2, 0, 1, 0, 12],
+  [2, 3, 0, 0, 1, 21],
+  [-3, -5, 0, 0, 0, 0]
 ]
+
+// Solução:
+// f1 = 2,50
+// x2 = 6
+// x1 = 1,50
+// z = 34,50
